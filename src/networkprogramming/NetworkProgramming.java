1bff36bd0ae4f2b9d61620fd42165094f886b975
alenpariyaroct10 <oct10.alenpariyar@gmail.com>
2025-01-29 08:13:34 +0545

    first commit

---
+++ src/networkprogramming/NetworkProgramming.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package networkprogramming;
+
+/**
+ *
+ * @author oct10
+ */
+public class NetworkProgramming {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
